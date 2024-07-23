032dd95c6b30356757d33878737edc392912d773
YongJu Lee <dydwn6565@naver.com>
2024-07-23 04:54:58 -0700

    added icons in footer

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom';
 
 import Header from './Header.jsx';
 import ActivityFeedPage from './MainPage.jsx';
+import Footer from './Footer.jsx';
 
 const App = () => {
   return (
     <div className='container'>
       <Header/>
       <ActivityFeedPage />
+      <Footer />
     </div>
   );
 };
---
+++ src/Footer.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { IoMdCall } from "react-icons/io";
+import { GoPerson } from "react-icons/go";
+import { VscSettingsGear } from "react-icons/vsc";
+import { PiDotsNineBold } from "react-icons/pi";
+import "./css/footer.css"
+const Footer = () => {
+  return (
+    <div className='footer-container'>
+      <IoMdCall />
+      <GoPerson />
+      <PiDotsNineBold />
+      <VscSettingsGear />
+      <div className='footer-color-indicator'></div>
+    </div>
+  )
+}
+
+export default Footer
